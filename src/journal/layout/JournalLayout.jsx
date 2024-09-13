e964424d40b42413fbf6f33a4379b320841bedf7
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-09-13 15:48:39 -0400

    SideBar

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,26 @@
+import { Box, Toolbar } from "@mui/material"
+import { NavBar, Sidebar } from "../components"
+
+
+const drawerWidth =280
+
+export const JournalLayout = ({children}) => {
+  return (
+    <Box sx={{display: 'flex'}}>
+        {/*Navbar */}
+        <NavBar drawerWidth={drawerWidth}/>
+
+        {/*Sidebar */}
+        <Sidebar drawerWidth={drawerWidth}/>
+        <Box
+            component='main'
+            sx={{flexGrow:1, p:3}}
+        >
+            <Toolbar />
+
+            {/*Toolbar */}
+            {children}
+        </Box>
+    </Box>
+  )
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,17 @@
 import { Typography } from "@mui/material"
 import { MailOutline } from "@mui/icons-material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-      <MailOutline />
-    </>
+    <JournalLayout>
+      <Typography> Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, iste rerum error debitis sequi repellendus numquam tenetur magni iure, ratione laborum aperiam dolor voluptates. Quis illo odit quas repellendus illum?
+
+      {/*NothinSelected */}
+      {/*NoteView */}
+
+      </Typography>
+    </JournalLayout>
   )
 }
