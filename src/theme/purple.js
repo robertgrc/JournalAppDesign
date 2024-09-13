56ea3d43e36dfcc68a8e8f1900d904033f2d28f9
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-09-13 08:56:24 -0400

    LoginPage sin Layout

--- src/theme/purple.js
+++ src/theme/purple.js
@@ -6,10 +6,10 @@ import { red } from '@mui/material/colors';
 export const purpleTheme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#262254',
     },
     secondary: {
-      main: '#19857b',
+      main: '#543884',
     },
     error: {
       main: red.A400,
