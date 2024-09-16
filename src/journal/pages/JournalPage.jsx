48382e39e4353d16ce7925c4a61d2b448c655cbd
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-09-16 14:41:37 -0400

    Agregando un boton

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,17 +1,33 @@
 
-import { Typography } from "@mui/material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NothingSelectedView } from "../views/NothingSelectedView"
 import { NoteView } from "../views/NoteView"
+import { AddOutlined } from "@mui/icons-material"
 
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <NothingSelectedView /> */}
+      <NothingSelectedView />
 
       {/*NoteView */}
-      <NoteView />
+      {/* <NoteView /> */}
+
+    <IconButton
+      size="large"
+      sx={{
+        color:'white',
+        backgroundColor:'error.main',
+        ':hover':{ backgroundColor:'error.main', opacity:0.9},
+        position: 'fixed',
+        right: 50,
+        bottom: 50
+      }}
+    >
+      <AddOutlined sx={{ fontSize: 30 }} />
+
+    </IconButton>
 
     </JournalLayout>
   )
